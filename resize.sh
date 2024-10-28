e54c8a95e972507fce8666241e9c3914deb5dbdd
vignesh nalla <vigneshnalla999@gmail.com>
2024-10-28 19:01:19 +0530

    added resize.sh to extend root and var size

---
+++ resize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Resize the partition
+sudo growpart /dev/nvme0n1 4
+
+# Extend the logical volume for root
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+
+# Extend the logical volume for /var
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+
+# Grow the XFS filesystem for root
+sudo xfs_growfs /
+
+# Grow the XFS filesystem for /var
+sudo xfs_growfs /var
