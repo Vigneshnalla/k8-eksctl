b916779b6e381f9af74022f0e4ff6e7b15d5602d
vignesh nalla <vigneshnalla999@gmail.com>
2024-10-30 18:31:24 +0530

    commented some code

--- install_and_create_eks_cluster.sh
+++ install_and_create_eks_cluster.sh
@@ -17,18 +17,18 @@ echo "kubectl installed successfully."
 # Verify kubectl version
 kubectl version --client
 
-# Granting all users access to kubectl (adjust permissions if needed)
-sudo chmod +x /usr/local/bin/kubectl
-echo "kubectl permissions adjusted for all users."
+# # Granting all users access to kubectl (adjust permissions if needed)
+# sudo chmod +x /usr/local/bin/kubectl
+# echo "kubectl permissions adjusted for all users."
 
-# EKS Cluster Creation
-CONFIG_FILE="eks.yaml"
+# # EKS Cluster Creation
+# CONFIG_FILE="eks.yaml"
 
-# Check if the config file exists in the same directory
-if [[ -f $CONFIG_FILE ]]; then
-    echo "Creating EKS cluster using $CONFIG_FILE..."
-    eksctl create cluster --config-file=$CONFIG_FILE
-else
-    echo "Config file $CONFIG_FILE not found in the current directory."
-    exit 1
-fi
+# # Check if the config file exists in the same directory
+# if [[ -f $CONFIG_FILE ]]; then
+#     echo "Creating EKS cluster using $CONFIG_FILE..."
+#     eksctl create cluster --config-file=$CONFIG_FILE
+# else
+#     echo "Config file $CONFIG_FILE not found in the current directory."
+#     exit 1
+# fi
